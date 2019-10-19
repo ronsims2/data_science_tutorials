50e37aaeb9cae10ccad9c2a81ed6e72096d3b1c6
Ronald Sims II <ron@orangemantis.net>
2019-10-19 17:33:51 -0400

    Initial commit ... chapter 1.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Tutorials for data science.',
+    author='RA Sims II',
+    license='',
+)
